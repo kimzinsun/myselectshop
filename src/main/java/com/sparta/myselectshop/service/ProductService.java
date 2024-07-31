fa660b6920f3c2bd6c5001bf30247cac8a7a1c05
Kim JinSeon <kwlstjs00@gmail.com>
2024-07-31 19:40:05 +0900

    feat: 회원별 상품 API 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,6 +4,7 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.entity.User;
 import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
@@ -19,8 +20,8 @@ public class ProductService {
     private final ProductRepository productRespository;
     public static final int MIN_MY_PRICE = 100;
 
-    public ProductResponseDto createProduct(ProductRequestDto requestDto) {
-        Product product = productRespository.save(new Product(requestDto));
+    public ProductResponseDto createProduct(ProductRequestDto requestDto, User user) {
+        Product product = productRespository.save(new Product(requestDto, user));
         return new ProductResponseDto(product);
     }
 
@@ -38,8 +39,8 @@ public class ProductService {
         return new ProductResponseDto(product);
     }
 
-    public List<ProductResponseDto> getProducts() {
-        List<Product> productList = productRespository.findAll();
+    public List<ProductResponseDto> getProducts(User user) {
+        List<Product> productList = productRespository.findAllByUser(user);
         List<ProductResponseDto> productResponseDtoList = new ArrayList<>();
         for (Product product : productList) {
             productResponseDtoList.add(new ProductResponseDto(product));
@@ -53,5 +54,14 @@ public class ProductService {
         Product product = productRespository.findById(id)
                 .orElseThrow(() -> new NullPointerException("해당 상품은 존재하지 않습니다."));
         product.updateByItemDto(itemDto);
+    }
+
+    public List<ProductResponseDto> getAllProducts() {
+        List<Product> productList = productRespository.findAll();
+        List<ProductResponseDto> productResponseDtoList = new ArrayList<>();
+        for (Product product : productList) {
+            productResponseDtoList.add(new ProductResponseDto(product));
+        }
+        return productResponseDtoList;
     }
 }
