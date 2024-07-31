bd292ad73d896eb1f015d3dfc2be84879ad37f07
Kim JinSeon <kwlstjs00@gmail.com>
2024-07-31 18:29:03 +0900

    feat: Scheduler 기능 구현

--- src/main/java/com/sparta/myselectshop/service/ProductService.java
+++ src/main/java/com/sparta/myselectshop/service/ProductService.java
@@ -4,6 +4,7 @@ import com.sparta.myselectshop.dto.ProductMypriceRequestDto;
 import com.sparta.myselectshop.dto.ProductRequestDto;
 import com.sparta.myselectshop.dto.ProductResponseDto;
 import com.sparta.myselectshop.entity.Product;
+import com.sparta.myselectshop.naver.dto.ItemDto;
 import com.sparta.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -45,5 +46,12 @@ public class ProductService {
         }
         return productResponseDtoList;
 
+    }
+
+    @Transactional
+    public void updateBySearch(Long id, ItemDto itemDto) {
+        Product product = productRespository.findById(id)
+                .orElseThrow(() -> new NullPointerException("해당 상품은 존재하지 않습니다."));
+        product.updateByItemDto(itemDto);
     }
 }
